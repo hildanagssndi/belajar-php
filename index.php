c224ce1bd1ba38cdf163ee5ba2e066af0a7ca113
Hildan Agussandi <hildanagussandi16@gmail.com>
2022-12-06 12:08:21 +0700

    belajar-php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "Hello World! <br>";
+
+$nama = "Hildan Agussandi";
+$umur = 21;
+
+echo "Nama saya $nama, saya berusia $umur Tahun.<br>";
+
+$namaAdik = "Nazwa";
+$umurAdik = "17";
+
+echo "Nama adik saya adalah $namaAdik, adik saya berusia $umurAdik Tahun.<br>";
+
+$selisihumur = $umur - $umurAdik;
+
+echo "Selisih usia saya dengan adik saya adalah $selisihumur tahun.";
+
+?>
