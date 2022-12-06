34f7bad180a1a0d826630768697e9b3171fbe1d9
Hildan Agussandi <hildanagussandi16@gmail.com>
2022-12-06 14:56:57 +0700

    praktik materi php lanjutan

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "Hello World! <br>";
+echo "Hello World! <br><br>";
 
 $nama = "Hildan Agussandi";
 $umur = 21;
@@ -14,6 +14,25 @@ echo "Nama adik saya adalah $namaAdik, adik saya berusia $umurAdik Tahun.<br>";
 
 $selisihumur = $umur - $umurAdik;
 
-echo "Selisih usia saya dengan adik saya adalah $selisihumur tahun.";
+echo "Selisih usia saya dengan adik saya adalah $selisihumur tahun.<br><br>";
+
+echo "Perulangan <br><br>";
+
+$nom = 20;
+for ($i=0; $i<$nom; $i++){
+    $n = $i + 1;
+    echo $n." ". $nama."<br/>";
+}
+
+echo "<br/> Do-While <br><br>";
+
+$no = 5;
+$i = 0;
+
+do {
+    $n = $i + 1;
+    echo $n." ". $nama."<br/>";
+    $i++;
+} while ($i < $no)
 
 ?>
